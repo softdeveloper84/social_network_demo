89322bb704fc417c207d9260853da88e890fd976
user <user@user.com>
2020-04-09 03:46:37 +0300

    12 lesson

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR4pKTK4hGvAxEjxqDWily0XOFIewS2egEfuShOwOmrYn1L8Tzs&usqp=CAU"/>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="nav">
+            <div>
+                <a href="">Messages</a>
+            </div>
+            <div>
+                <a href="">News</a>
+            </div>
+            <div>
+                <a href="">Music</a>
+            </div>
+            <div>
+                <a href="">Settings</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
