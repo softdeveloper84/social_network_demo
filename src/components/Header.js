21b051e0b75da817d131579f9b7980aaf202a1b9
user <user@user.com>
2020-04-09 05:24:06 +0300

    14 - css module

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import s from './Header.module.css'
 
 const Header = () => {
     return (
-        <header className="header">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR4pKTK4hGvAxEjxqDWily0XOFIewS2egEfuShOwOmrYn1L8Tzs&usqp=CAU"/>
+        <header className={s.header}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR4pKTK4hGvAxEjxqDWily0XOFIewS2egEfuShOwOmrYn1L8Tzs&usqp=CAU" alt=""/>
         </header>
     );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import s from './Navbar.module.css'
+
 
 const Navbar = () => {
     return (
-        <nav className="nav">
-            <div>
-                <a href="">Messages</a>
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <a href="https://google.com">Messages</a>
             </div>
-            <div>
-                <a href="">News</a>
+            <div className={`${s.item} ${s.active}`}>
+                <a href="https://google.com">News</a>
             </div>
-            <div>
-                <a href="">Music</a>
+            <div className={s.item}>
+                <a href="https://google.com">Music</a>
             </div>
-            <div>
-                <a href="">Settings</a>
+            <div className={s.item}>
+                <a href="https://google.com">Settings</a>
             </div>
         </nav>
     );
